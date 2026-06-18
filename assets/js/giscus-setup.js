3256809f254e974c999e044a65718ea22605b5cd
pauljparks <pauljparks@users.noreply.github.com>
2026-06-18 10:47:56 +0000

    deploy: 3c21d923114f3f582f0dcc6f5f0916343550e7e0

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "pauljparks/pauljparks.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
